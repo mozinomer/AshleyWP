f206055c4ab18bbf2e94795a897248a03537ee13
Mozin Omer <mozinomer12@gmail.com>
2020-05-14 14:36:17 +0500

    making the footer of the site

---
+++ footer.php
@@ -0,0 +1,20 @@
+
+<footer id="mainFooter">
+	<div class="minimumContainer">
+		<h4 class="HeadingFooter">
+			Featured Community
+		</h4>
+		<ul>
+			<li><a href="">Park City </a></li>
+			<li><a href="">Utah</a></li>
+			<li><a href="">Deer Valley</a></li>
+			<li><a href="">The Canyons</a></li>
+			<li><a href="">Glenwild</a></li>
+			<li><a href="">Promontory</a></li>
+		</ul>
+	</div>
+</footer>
+
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/style.css">
+</head>
+<?php wp_head(); ?>
+<body>
