0136cda2be32a4d76aa29df323d56bf770252956
Mozin Omer <mozinomer12@gmail.com>
2020-05-14 15:05:59 +0500

    made the footer

--- footer.php
+++ footer.php
@@ -1,17 +1,34 @@
 
 <footer id="mainFooter">
 	<div class="minimumContainer">
-		<h4 class="HeadingFooter">
-			Featured Community
-		</h4>
-		<ul>
-			<li><a href="">Park City </a></li>
-			<li><a href="">Utah</a></li>
-			<li><a href="">Deer Valley</a></li>
-			<li><a href="">The Canyons</a></li>
-			<li><a href="">Glenwild</a></li>
-			<li><a href="">Promontory</a></li>
+		<?php if( get_field('footer_logo', 'option') ) { ?>
+		    <img src="<?php the_field('footer_logo', 'option'); ?>" class="footerLogo" />
+		<?php } else { ?>
+			<h4 class="HeadingFooter">
+				<?php the_field('alternate_text_heading', 'option'); ?>
+			</h4>
+		<?php } ?>
+		<?php 
+			wp_nav_menu( array( 
+		    'theme_location' => 'footerMenu', 
+		    'container_class' => 'footerMenu' ) ); 
+		?>
+		<ul class="Policies">
+			<li>
+				<a href="#">
+					<?php the_field('privacy_policy_text', 'option'); ?>
+				</a>
+			</li>
+			<li>
+				<a href="#">
+					 |  
+					<?php the_field('terms_&_condition_text', 'option'); ?>
+				</a>
+			</li>
 		</ul>
+		<p class="copyRights">
+			<?php the_field('copy_rights_text', 'option'); ?>
+		</p>
 	</div>
 </footer>
 
---
+++ functions.php
@@ -0,0 +1,36 @@
+<?php
+
+if( function_exists('acf_add_options_page') ) {
+	
+	acf_add_options_page(array(
+		'page_title' 	=> 'Theme General Settings',
+		'menu_title'	=> 'Theme Settings',
+		'menu_slug' 	=> 'theme-general-settings',
+		'capability'	=> 'edit_posts',
+		'redirect'		=> false
+	));
+	
+	acf_add_options_sub_page(array(
+		'page_title' 	=> 'Theme Header Settings',
+		'menu_title'	=> 'Header',
+		'parent_slug'	=> 'theme-general-settings',
+	));
+	
+	acf_add_options_sub_page(array(
+		'page_title' 	=> 'Theme Footer Settings',
+		'menu_title'	=> 'Footer',
+		'parent_slug'	=> 'theme-general-settings',
+	));
+	
+}
+
+
+function wpb_custom_new_menu() {
+  register_nav_menus(
+    array(
+      'footerMenu' => __( 'Footer Menu' ),
+      'headerMenu' => __( 'header Menu' )
+    )
+  );
+}
+add_action( 'init', 'wpb_custom_new_menu' );
